8a46b5a1dbed936c38ac12791eba4ebe9037cdef
tom-2810 <tom.poortman@hva.nl>
2023-08-21 22:17:14 +0200

    dynamic project route
    fallback to 'default' project if projectId is 'doesnotexist'

---
+++ src/routes/projects/[[projectId]]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { page } from '$app/stores'
+
+    let { projectId = 'default' } = $page.params
+
+    if(projectId == 'doesnotexist')
+    projectId = 'default'
+
+</script>
+
+<h1>this is project { projectId }</h1>
